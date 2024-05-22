8d8faf53520a64573ab2d28946104e56996948c4
MaryamArs <mm6525976@gmail.com>
2024-05-22 18:56:14 +0500

    y

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   
+    $(".plus-icon").click(function(){
+    $(this).toggleClass(' minus-icon');
+    $(this).toggleClass(".come") 
+    
+    
+  });
+  
+
+  });
